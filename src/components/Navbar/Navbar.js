97a92dd3df9b7f6a08aa7f07ea2e92e5faf339ac
tarunkumar575022 <tarunkumar575022@gmail.com>
2025-09-26 14:47:28 +0530

    Update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
     <div>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand navbarspace" href="#">Navbar</a>
+    <a class="navbar-brand navbarspace" href="#">Foodieland.</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -16,13 +16,16 @@ const Navbar = () => {
           <a class="nav-link active " aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item ms-3">
-          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="#">Recipies</a>
         </li>
         <li class="nav-item ms-3">
-          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link" href="#">Blog</a>
         </li>
         <li class="nav-item ms-3">
-          <a class=" navbarspace nav-link disabled items" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        <li class="nav-item ms-3">
+          <a class=" navbarspace nav-link disabled items" href="#" tabindex="-1" aria-disabled="true">About us</a>
         </li>
       </ul>
       <div className='spacee'>
